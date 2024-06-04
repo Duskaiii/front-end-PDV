d44f71022685d0990a88d58d89d9b5eefac5a01e
raelcantu <111098935+raelcantu@users.noreply.github.com>
2024-06-04 02:29:43 -0300

    Log de consumo da api

---
+++ ProjetoIntegrado/src/main/java/models/Produto.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author rafae
+ */
+public class Produto {
+    
+    private Long id;
+    String descricao;
+    Double preco;
+    String categoria;
+
+    public Produto() {
+    }
+
+    public Produto(Long id, String descricao, Double preco, String categoria) {
+        this.id = id;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    
+    
+}
---
+++ ProjetoIntegrado/src/main/java/models/Venda.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+import java.util.Date;
+
+/**
+ *
+ * @author rafae
+ */
+public class Venda {
+    
+    private Long id;
+    private Date data;
+    private Cliente cliente;
+    private Double valorTotal;
+    private String observacoes;
+
+    public Venda() {
+    }
+
+    public Venda(Long id, Date data, Cliente cliente, Double valorTotal, String observacoes) {
+        this.id = id;
+        this.data = data;
+        this.cliente = cliente;
+        this.valorTotal = valorTotal;
+        this.observacoes = observacoes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+    
+    
+    
+}
