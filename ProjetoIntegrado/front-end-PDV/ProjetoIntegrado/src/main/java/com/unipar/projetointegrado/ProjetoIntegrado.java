fcd883332563d2d845d1f4fab975e68cf447e542
raelcantu <111098935+raelcantu@users.noreply.github.com>
2024-06-04 02:25:15 -0300

    Log de consumo da api

---
+++ ProjetoIntegrado/front-end-PDV/ProjetoIntegrado/src/main/java/com/unipar/projetointegrado/ProjetoIntegrado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.unipar.projetointegrado;
+
+/**
+ *
+ * @author Dzkyy
+ */
+public class ProjetoIntegrado {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ProjetoIntegrado/front-end-PDV/ProjetoIntegrado/src/main/java/models/Cliente.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author rafae
+ */
+
+public class Cliente {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String nome, String cpf, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
