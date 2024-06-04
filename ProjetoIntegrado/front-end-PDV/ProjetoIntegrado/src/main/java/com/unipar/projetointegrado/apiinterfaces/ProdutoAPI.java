e6d38bbf0163389dde8e67d33130b3b889efc4f8
raelcantu <111098935+raelcantu@users.noreply.github.com>
2024-06-04 02:27:15 -0300

    Log de consumo da api

---
+++ ProjetoIntegrado/front-end-PDV/ProjetoIntegrado/src/main/java/com/unipar/projetointegrado/apiinterfaces/ProdutoAPI.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.unipar.projetointegrado.apiinterfaces;
+
+import java.util.List;
+import models.Produto;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+/**
+ *
+ * @author rafae
+ */
+public interface ProdutoAPI {
+    
+    @GET("/produto/all")
+    Call<List<Produto>> findAll();
+    
+}
---
+++ ProjetoIntegrado/front-end-PDV/ProjetoIntegrado/src/main/java/com/unipar/projetointegrado/util/LogConsumoAPI.java
@@ -0,0 +1,51 @@
+package com.unipar.projetointegrado.util;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class LogConsumoAPI {
+
+    public void registrarConsumo(Long data, String tipoConsumo, String status) throws IOException {
+
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dataAtual = dateFormat. format(data);
+
+        File log = new File("ApiLogs/logs.txt");
+        File dir = new File("ApiLogs");
+
+        dir.mkdirs();
+
+        try{
+            if(log.exists()==false){
+                log.createNewFile();
+                try (FileWriter escritor = new FileWriter("ApiLogs/logs.txt"); BufferedWriter buff = new BufferedWriter(escritor)) {
+
+                    buff.write("Data: " + dataAtual + "\n");
+                    buff.write(tipoConsumo + "\n");
+                    buff.write("Status: " + status + "\n");
+
+
+                } catch (IOException e) {
+                    System.out.println("erro: " + e.getMessage());
+                }
+            } else {
+                PrintWriter out = new PrintWriter(new FileWriter(log, true));
+                out.append("**************************" + "\n" + "Data: " + dataAtual + "\n" +
+                        tipoConsumo + "\n" + "Status: " + status + "\n");
+                out.close();
+            }
+        }catch(IOException e){
+            System.out.println("COULD NOT LOG!!");
+        }
+
+
+
+    }
+
+}
